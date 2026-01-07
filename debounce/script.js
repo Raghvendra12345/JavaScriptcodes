5329028bde1f7ea59ee3d4fa3a650eb5d2f7b21d
Raghvendra12345 <103117995+Raghvendra12345@users.noreply.github.com>
2026-01-07 13:06:16 +0530

    complete debounce

--- debounce/script.js
+++ debounce/script.js
@@ -1,2 +1,25 @@
+let count=0;
+let time;
 const  searchh=document.getElementById("searchinput")
 const resultt=document.getElementById("resultbox")
+
+searchh.addEventListener("input",function(event){
+    const query=event.target.value.trim();
+
+clearTimeout(time)
+
+time=setTimeout(()=>{
+
+
+    if(query!="password"){
+        resultt.textContent="Wrong password attempt "+ ++count;
+        resultt.style.color="red"
+    }
+    else{
+        resultt.textContent="Right"
+        resultt.style.color="green"
+        count=0
+    }
+},1000)
+
+});

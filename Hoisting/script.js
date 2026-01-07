3e65571d28c50ca235f0b316a5665138479c3cae
Raghvendra12345 <103117995+Raghvendra12345@users.noreply.github.com>
2026-01-07 13:56:21 +0530

    execution and hoisting

---
+++ Hoisting/script.js
@@ -0,0 +1,20 @@
+// console.log(a)
+// var a=5;
+
+
+// fucc("Hello")
+// console.log(a)
+// var a=5;
+
+// function fucc(name){
+//     console.log(name)
+// }
+
+
+console.log(fucc)
+console.log(a)
+var a=5;
+
+var fucc=(name)=>{
+    console.log(name)
+}                         ///undefined   undefined
